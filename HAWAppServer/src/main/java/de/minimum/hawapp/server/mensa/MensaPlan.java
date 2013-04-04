64230fe640471c32716e2efeb68837735675a977
svenallers89@googlemail.com <svenallers89@googlemail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-04-04 15:52:29 +0000

    Mensaplan/jSoup eingebunden

---
+++ HAWAppServer/src/main/java/de/minimum/hawapp/server/mensa/MensaPlan.java
@@ -0,0 +1,32 @@
+package de.minimum.hawapp.server.mensa;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public interface MensaPlan {
+
+    /**
+     * Parst die Webseite neu
+     * 
+     * @throws IOException
+     *             , bei keiner Verbindung zur Mensa Seite
+     */
+    public void update() throws IOException;
+
+    /**
+     * Gibt den Essensplan eines speziellen Tages als Liste zur�ck.
+     * 
+     * @param Enum
+     *            day
+     * @return Liste der Gerichte
+     */
+    public List<Meal> getDayPlan(String day);
+
+    /**
+     * Gibt die aktuelle Essensliste der Woche zur�ck;
+     * 
+     * @return Map mit Enums f�r den Tag und Listen der Gerichte
+     */
+    public Map<String, List<Meal>> getWeekPlan();
+}
