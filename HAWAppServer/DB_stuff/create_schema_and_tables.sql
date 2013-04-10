40aa62d37f82603b208eeba8e16e6c8f845585b2
jannik.graw.hh@gmail.com <jannik.graw.hh@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-04-10 20:15:04 +0000

    sql script zum konfigurieren der db

---
+++ HAWAppServer/DB_stuff/create_schema_and_tables.sql
@@ -0,0 +1,210 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+
+
+DROP SCHEMA IF EXISTS `haw_app` ;
+
+CREATE SCHEMA IF NOT EXISTS `haw_app` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+
+USE `haw_app` ;
+
+
+
+-- -----------------------------------------------------
+
+-- Table `haw_app`.`Semester`
+
+-- -----------------------------------------------------
+
+CREATE  TABLE IF NOT EXISTS `haw_app`.`Semester` (
+
+  `uuid` VARCHAR(255) NOT NULL DEFAULT '' ,
+
+  `begin` DATE NULL ,
+
+  `end` DATE NULL ,
+
+  `lastModified` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+
+  PRIMARY KEY (`uuid`) )
+
+ENGINE = InnoDB;
+
+
+
+
+
+-- -----------------------------------------------------
+
+-- Table `haw_app`.`Category`
+
+-- -----------------------------------------------------
+
+CREATE  TABLE IF NOT EXISTS `haw_app`.`Category` (
+
+  `uuid` VARCHAR(255) NOT NULL DEFAULT '' ,
+
+  `name` VARCHAR(255) NULL ,
+
+  `lastModified` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+
+  `Semester_uuid` VARCHAR(255) NOT NULL ,
+
+  PRIMARY KEY (`uuid`) ,
+
+  INDEX `fk_Category_Semester_idx` (`Semester_uuid` ASC) ,
+
+  CONSTRAINT `fk_Category_Semester`
+
+    FOREIGN KEY (`Semester_uuid` )
+
+    REFERENCES `haw_app`.`Semester` (`uuid` )
+
+    ON DELETE NO ACTION
+
+    ON UPDATE NO ACTION)
+
+ENGINE = InnoDB;
+
+
+
+
+
+-- -----------------------------------------------------
+
+-- Table `haw_app`.`Lecture`
+
+-- -----------------------------------------------------
+
+CREATE  TABLE IF NOT EXISTS `haw_app`.`Lecture` (
+
+  `uuid` VARCHAR(255) NOT NULL ,
+
+  `lastModified` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+
+  `Category_uuid` VARCHAR(255) NOT NULL ,
+
+  `name` VARCHAR(255) NULL ,
+
+  `lecturerName` VARCHAR(255) NULL ,
+
+  PRIMARY KEY (`uuid`) ,
+
+  INDEX `fk_Lecture_Category1_idx` (`Category_uuid` ASC) ,
+
+  CONSTRAINT `fk_Lecture_Category1`
+
+    FOREIGN KEY (`Category_uuid` )
+
+    REFERENCES `haw_app`.`Category` (`uuid` )
+
+    ON DELETE NO ACTION
+
+    ON UPDATE NO ACTION)
+
+ENGINE = InnoDB;
+
+
+
+
+
+-- -----------------------------------------------------
+
+-- Table `haw_app`.`Appointment`
+
+-- -----------------------------------------------------
+
+CREATE  TABLE IF NOT EXISTS `haw_app`.`Appointment` (
+
+  `uuid` VARCHAR(255) NOT NULL ,
+
+  `name` VARCHAR(255) NULL ,
+
+  `begin` DATETIME NULL ,
+
+  `end` DATETIME NULL ,
+
+  `location` VARCHAR(255) NULL ,
+
+  `details` VARCHAR(1000) NULL ,
+
+  `lastModified` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+
+  `Lecture_uuid` VARCHAR(255) NOT NULL ,
+
+  PRIMARY KEY (`uuid`) ,
+
+  INDEX `fk_Appointment_Lecture1_idx` (`Lecture_uuid` ASC) ,
+
+  CONSTRAINT `fk_Appointment_Lecture1`
+
+    FOREIGN KEY (`Lecture_uuid` )
+
+    REFERENCES `haw_app`.`Lecture` (`uuid` )
+
+    ON DELETE NO ACTION
+
+    ON UPDATE NO ACTION)
+
+ENGINE = InnoDB;
+
+
+
+
+
+-- -----------------------------------------------------
+
+-- Table `haw_app`.`ChangeMessage`
+
+-- -----------------------------------------------------
+
+CREATE  TABLE IF NOT EXISTS `haw_app`.`ChangeMessage` (
+
+  `uuid` VARCHAR(255) NOT NULL ,
+
+  `lastModified` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+
+  `changeat` DATETIME NULL ,
+
+  `reason` VARCHAR(1000) NULL ,
+
+  `what` VARCHAR(1000) NULL ,
+
+  `person` VARCHAR(255) NULL ,
+
+  `Lecture_uuid` VARCHAR(255) NOT NULL ,
+
+  PRIMARY KEY (`uuid`) ,
+
+  INDEX `fk_Change_Lecture1_idx` (`Lecture_uuid` ASC) ,
+
+  CONSTRAINT `fk_Change_Lecture1`
+
+    FOREIGN KEY (`Lecture_uuid` )
+
+    REFERENCES `haw_app`.`Lecture` (`uuid` )
+
+    ON DELETE NO ACTION
+
+    ON UPDATE NO ACTION)
+
+ENGINE = InnoDB;
+
+
+
+USE `haw_app` ;
+
+
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
