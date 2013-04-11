530996511a88025d1b3350ec5fde5770f7f418c1
jannik.graw.hh@gmail.com <jannik.graw.hh@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-04-11 18:08:51 +0000

    ServiceStartRule für JUnit

---
+++ trunk/HAWAppServer/src/test/java/de/minimum/hawapp/test/rest/TestWithServer.java
@@ -0,0 +1,34 @@
+package de.minimum.hawapp.test.rest;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+import de.minimum.hawapp.test.util.ServerStartRule;
+
+public class TestWithServer {
+	/*
+	 * Die Enviroment Variable TOMCAT_HOME muss gesetzt sein!
+	 */
+	@ClassRule
+	public static TestRule rule=new ServerStartRule();
+	
+	@Test
+	public void test() throws IOException {
+		URL  url=new URL ("http://localhost:8080/server/rest/testservice");
+		InputStream input=url.openStream();
+		int i=input.read();
+		while(i!=-1){
+			System.out.print((char)i);
+			i=input.read();
+		}
+	}
+
+}
