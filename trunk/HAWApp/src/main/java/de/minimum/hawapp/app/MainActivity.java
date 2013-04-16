52570fe6a7f3bdfd1eed0678553fd3d3316f47cc
w.timon@gmail.com <w.timon@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-04-16 17:49:17 +0000

    GUI: navi + anmeldung

--- trunk/HAWApp/src/main/java/de/minimum/hawapp/app/MainActivity.java
+++ trunk/HAWApp/src/main/java/de/minimum/hawapp/app/MainActivity.java
@@ -1,45 +1,55 @@
 package de.minimum.hawapp.app;
 
-import android.app.Activity;
-import android.view.Menu;
-import android.widget.TextView;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
 
-import com.googlecode.androidannotations.annotations.AfterViews;
-import com.googlecode.androidannotations.annotations.Background;
-import com.googlecode.androidannotations.annotations.EActivity;
-import com.googlecode.androidannotations.annotations.UiThread;
-import com.googlecode.androidannotations.annotations.ViewById;
-import com.googlecode.androidannotations.annotations.rest.RestService;
-
-import de.minimum.hawapp.app.rest.RestClient;
-
-@EActivity(R.layout.activity_main)
-public class MainActivity extends Activity {
-
-    @ViewById
-    TextView hello;
-    @RestService
-    RestClient restClient;
-
-    @AfterViews
-    void afterViews() {
-    }
-
-    @UiThread
-    void doSomethingElseOnUiThread() {
-        this.hello.setText("Hi!");
-    }
-
-    @Background
-    void doSomethingInBackground() {
-        this.restClient.main();
-        doSomethingElseOnUiThread();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater();
-        return true;
-    }
-
+public class MainActivity extends TabActivity {
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.main);
+	 
+	    Resources res = getResources(); // Resource object to get Drawables
+	    TabHost tabHost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+	    Intent intent;  // Reusable Intent for each tab
+	 
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    intent = new Intent().setClass(this, LoginActivity_.class);
+	 
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabHost.newTabSpec("artists").setIndicator("",
+	                      res.getDrawable(R.drawable.ic_tab_anmeldung))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+	 
+	    // Do the same for the other tabs
+	    intent = new Intent().setClass(this, MensaActivity.class);
+	    spec = tabHost.newTabSpec("albums").setIndicator("",
+	                      res.getDrawable(R.drawable.ic_tab_mensa))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+	 
+	    intent = new Intent().setClass(this, StiSysActivity.class);
+	    spec = tabHost.newTabSpec("songs").setIndicator("",
+	                      res.getDrawable(R.drawable.ic_tab_kalender))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+	    
+	    intent = new Intent().setClass(this, KalenderActivity.class);
+	    spec = tabHost.newTabSpec("songs").setIndicator("",
+	                      res.getDrawable(R.drawable.ic_tab_kalender))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+	    
+	    intent = new Intent().setClass(this, SchwarzesBrettActivity.class);
+	    spec = tabHost.newTabSpec("songs").setIndicator("",
+	                      res.getDrawable(R.drawable.ic_tab_kalender))
+	                  .setContent(intent);
+	    tabHost.addTab(spec);
+	 
+	    tabHost.setCurrentTab(0);
+	}
 }
