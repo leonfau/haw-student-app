23baaafb1cfc7a23506679d6ae742965d7b815f7
w.timon@gmail.com <w.timon@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-04-17 00:15:23 +0000

    GUI: stisys

--- HAWApp/src/main/java/de/minimum/hawapp/app/StiSysActivity.java
+++ HAWApp/src/main/java/de/minimum/hawapp/app/StiSysActivity.java
@@ -1,15 +1,61 @@
 package de.minimum.hawapp.app;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
 
-public class StiSysActivity extends Activity {
-	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+import com.googlecode.androidannotations.annotations.Click;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
  
-        TextView textview = new TextView(this);
-        textview.setText("This is the StiSysActivity tab");
-        setContentView(textview);
+import android.app.ExpandableListActivity;
+import android.os.Bundle;
+import android.widget.ExpandableListAdapter;
+import android.widget.SimpleExpandableListAdapter;
+
+public class StiSysActivity extends ExpandableListActivity {
+    private static final String NAME = "NAME";
+    private static final String IS_EVEN = "IS_EVEN";
+     
+    private ExpandableListAdapter mAdapter;
+     
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        List<Map<String, String>> groupData = new ArrayList<Map<String, String>>();
+        List<List<Map<String, String>>> childData = new ArrayList<List<Map<String, String>>>();
+        for (int i = 0; i < 6; i++) {
+            Map<String, String> curGroupMap = new HashMap<String, String>();
+            groupData.add(curGroupMap);
+            curGroupMap.put(NAME, "B-AI " + (i+1));
+           // curGroupMap.put(IS_EVEN, (i % 2 == 0) ? "This group is even" : "This group is odd");
+             
+            List<Map<String, String>> children = new ArrayList<Map<String, String>>();
+            for (int j = 0; j < 5; j++) {
+                Map<String, String> curChildMap = new HashMap<String, String>();
+                children.add(curChildMap);
+                curChildMap.put(NAME, "Praktikum Graphentheoretische Konzepte und Algorithmen (GKAP)");
+                curChildMap.put(IS_EVEN, "18.01.2013 Klauck (KLC) 		Ergebnis: pvl");
+            }
+            childData.add(children);
+        }
+         
+        // Set up our adapter
+        mAdapter = new SimpleExpandableListAdapter(
+                this,
+                groupData,
+                R.layout.simple_expandable_list_item_1,
+                new String[] { NAME, IS_EVEN },
+                new int[] { android.R.id.text1, android.R.id.text2 },
+                childData,
+                R.layout.simple_expandable_list_item_2,
+                new String[] { NAME, IS_EVEN },
+                new int[] { android.R.id.text1, android.R.id.text2 }
+                );
+        setListAdapter(mAdapter);
     }
+ 
 }
