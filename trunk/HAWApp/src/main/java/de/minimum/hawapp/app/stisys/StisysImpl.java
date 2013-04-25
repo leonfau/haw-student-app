390d2ec001b81e3b54bf14971d239fa66ca7b6a8
blubbay@gmail.com <blubbay@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-04-25 17:57:25 +0000

    StISys login Methode hinzugefuegt

--- trunk/HAWApp/src/main/java/de/minimum/hawapp/app/stisys/StisysImpl.java
+++ trunk/HAWApp/src/main/java/de/minimum/hawapp/app/stisys/StisysImpl.java
@@ -1,6 +1,54 @@
 package de.minimum.hawapp.app.stisys;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class StisysImpl implements Stisys {
+    static final String STISYS_URL = "https://stisys.informatik.haw-hamburg.de/stisys/";
+
+    @Override
+    public String login(final String name, final String password) {
+        HttpsURLConnection conn = null;
+        List<String> cookies = null;
+
+        try {
+            final URL url = new URL(STISYS_URL + "login.do");
+            final String param = "username=" + URLEncoder.encode(name, "UTF-8") + "&password="
+                            + URLEncoder.encode(password, "UTF-8");
+
+            conn = (HttpsURLConnection)url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setFixedLengthStreamingMode(param.getBytes().length);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+            final PrintWriter out = new PrintWriter(conn.getOutputStream());
+            out.print(param);
+            out.close();
+            cookies = conn.getHeaderFields().get("Set-Cookie");
+        }
+        catch(final MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch(final UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        catch(final IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return cookies.toString();
+    }
 
 }
