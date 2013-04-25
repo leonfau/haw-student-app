096e2d147865f040496625e98c002216906c1b5e
blubbay@gmail.com <blubbay@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-04-25 17:57:25 +0000

    StISys login Methode hinzugefuegt

--- HAWApp/src/main/java/de/minimum/hawapp/app/stisys/Stisys.java
+++ HAWApp/src/main/java/de/minimum/hawapp/app/stisys/Stisys.java
@@ -2,4 +2,15 @@ package de.minimum.hawapp.app.stisys;
 
 public interface Stisys {
 
+    /**
+     * Login-Methode fuer StISys, es wird der Cookie in Form von einer
+     * Session-ID zurueck gegeben. Sollte kein Cookie gesetzt sein ist die
+     * rueckgabe "null".
+     * 
+     * @param name
+     * @param password
+     * @return cookie
+     */
+    public String login(String name, String password);
+
 }
