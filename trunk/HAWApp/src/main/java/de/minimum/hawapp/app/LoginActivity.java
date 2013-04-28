6ec5ae215678ea5edc2f2ed0a3ff3a3c7a221a89
blubbay@gmail.com <blubbay@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-04-28 19:38:29 +0000

    Login Funktion umgestellt und Stisys angepasst...

--- trunk/HAWApp/src/main/java/de/minimum/hawapp/app/LoginActivity.java
+++ trunk/HAWApp/src/main/java/de/minimum/hawapp/app/LoginActivity.java
@@ -1,9 +1,9 @@
 package de.minimum.hawapp.app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
@@ -24,15 +24,21 @@ public class LoginActivity extends Activity {
 
     @Click(R.id.buttonLogin)
     public void login() {
-        // Intent i = new Intent(LoginActivity.this, MainActivity.class);
-        // startActivity(i);
-
-        final Intent i = new Intent(LoginActivity.this, StisysActivity.class);
-        i.putExtra("login", editLogin.getText().toString());
-        i.putExtra("password", editPassword.getText().toString());
+        // final Intent i = new Intent(LoginActivity.this,
+        // StisysActivity.class);
+        // i.putExtra("login", editLogin.getText().toString());
+        // i.putExtra("password", editPassword.getText().toString());
         // Log.i("LoginActivity", "Login: " + login.getText());
         // Log.i("LoginActivity", "Password: " + password.getText());
-        startActivity(i);
+        // startActivity(i);
+
+        StisysActivity.login = editLogin.getText().toString();
+        StisysActivity.password = editPassword.getText().toString();
+
+        Toast.makeText(getApplicationContext(), "Logindata setted", Toast.LENGTH_SHORT).show();
+
+        editLogin.setText("");
+        editPassword.setText("");
     }
 
 }
