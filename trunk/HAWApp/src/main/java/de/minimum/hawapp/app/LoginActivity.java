14e7baf43eeaddeb32a1a46d2ad2b296cea19784
blubbay@gmail.com <blubbay@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-04-28 18:51:23 +0000

    Stisys anpassungen

--- trunk/HAWApp/src/main/java/de/minimum/hawapp/app/LoginActivity.java
+++ trunk/HAWApp/src/main/java/de/minimum/hawapp/app/LoginActivity.java
@@ -1,34 +1,38 @@
 package de.minimum.hawapp.app;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
 
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
-
-import android.app.ActionBar;
-import android.app.ActionBar.Tab;
-import android.app.ActionBar.TabListener;
-import android.app.Activity;
-import android.app.FragmentTransaction;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.TabHost;
-import android.widget.Toast;
-import android.widget.TabHost.TabSpec;
+import com.googlecode.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.login)
-public class LoginActivity extends Activity{
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	};
-	
-	
-	@Click(R.id.btnLogin)
-	public void login(){
-						//		Intent i = new Intent(LoginActivity.this, MainActivity.class);
-						//        startActivity(i);
-	}
+public class LoginActivity extends Activity {
+    @ViewById
+    EditText editLogin;
+
+    @ViewById
+    EditText editPassword;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    };
+
+    @Click(R.id.buttonLogin)
+    public void login() {
+        // Intent i = new Intent(LoginActivity.this, MainActivity.class);
+        // startActivity(i);
+
+        final Intent i = new Intent(LoginActivity.this, StisysActivity.class);
+        i.putExtra("login", editLogin.getText().toString());
+        i.putExtra("password", editPassword.getText().toString());
+        // Log.i("LoginActivity", "Login: " + login.getText());
+        // Log.i("LoginActivity", "Password: " + password.getText());
+        startActivity(i);
+    }
 
 }
