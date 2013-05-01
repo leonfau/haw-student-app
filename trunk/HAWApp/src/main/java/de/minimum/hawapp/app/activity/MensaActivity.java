125c2d8753ce6a51a697b006259bcb5b019f613f
w.timon@gmail.com <w.timon@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-05-01 15:34:58 +0000

    GUI-Mensa: rating

--- trunk/HAWApp/src/main/java/de/minimum/hawapp/app/activity/MensaActivity.java
+++ trunk/HAWApp/src/main/java/de/minimum/hawapp/app/activity/MensaActivity.java
@@ -11,24 +11,27 @@ import de.minimum.hawapp.app.mensa.management.MensaManager;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.SimpleAdapter;
 
 public class MensaActivity extends NetworkingActivity {
-    private static final String NAME = "NAME";
     private static final String BESCHREIBUNG = "DESCRIPTION";
     private static final String PREIS = "PREIS";
-
+    private static final String RATING = "RATING";
+    ArrayList<HashMap<String, Object>> myArrList;
 	private ArrayList<HashMap<String, String>>  mylist;
     
 	DayPlan mealsToday; //Wird vom Restservice befüllt beim get()
 	MensaManager manager;
+	private ListView listView;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main3);
+		setContentView(R.layout.activity_mensa);
 		manager = ManagerFactory.getManager(MensaManager.class);
 	}
 	
@@ -49,13 +52,13 @@ public class MensaActivity extends NetworkingActivity {
 
             @Override
             protected void onPostExecute(Void arg0) {
-               display();
                super.onPostExecute(arg0);
+               display();
             }
 
 			@Override
 			protected Void doInBackground(Void... arg0) {
-				mealsToday = manager.getPlanForToday();
+//				mealsToday = manager.getPlanForToday();
 				return null;
 			}
         }.execute();
@@ -63,22 +66,38 @@ public class MensaActivity extends NetworkingActivity {
 	
 	@Override
 	void display() {
-		ListView list = (ListView) findViewById(R.id.SCHEDULE);
-		mylist = new ArrayList<HashMap<String, String>>();
-		if (mealsToday != null) {
-    	for (Meal meal : mealsToday.getMeals()) {
-    		HashMap<String, String> map = new HashMap<String, String>();
-			map.put(NAME, "Name muss raus");
-			map.put(BESCHREIBUNG, meal.getDescription());
-			String price = String.valueOf(meal.getStudentPrice() + "€ / " + String.valueOf(meal.getOthersPrice() +"€")).replace('.', ',');
-			map.put(PREIS, price);
-			mylist.add(map);	
-    	}
-		ListAdapter mSchedule = new SimpleAdapter(this, mylist, R.layout.row, new String[] {
-				NAME, BESCHREIBUNG, PREIS }, new int[] { R.id.name,
-				R.id.beschreibung, R.id.preis});
-		list.setAdapter(mSchedule);
+		listView = (ListView)findViewById(R.id.mensaList);
+		myArrList = new ArrayList<HashMap<String,Object>>();
+		
+//GUI TEST	
+		for (int i = 0; i < 25; i++) {
+			HashMap<String, Object> e = new HashMap<String, Object>();
+			e.put("BESCHREIBUNG", "Broccoliauflauf mit gekochtem Schinken und Penne (1,2,3,4,14,20,22) Napoli Soße (19) "+i);
+			e.put("RATING", (i % 5)+"");		
+			e.put("PREIS", "10.50 € / 20.10 €");
+			myArrList.add(e);
 		}
+		
+//		for (Meal meal : mealsToday.getMeals()) {
+//			HashMap<String, Object> e = new HashMap<String, Object>();
+//			e.put(BESCHREIBUNG, meal.getDescription());
+//			e.put(RATING, meal.getRating()+"");	
+//			String price = String.valueOf(meal.getStudentPrice() + "€ / " + String.valueOf(meal.getOthersPrice() +"€")).replace('.', ',');
+//			e.put(PREIS, price);
+//			myArrList.add(e);
+//		}
+
+		listView.setAdapter(new MealAdapter(MensaActivity.this,myArrList));
 	}
+
+	
+//	private void updateNewRatingPoint(int position,String newRatingPoint){		
+//	    View v = listView.getChildAt(position - listView.getFirstVisiblePosition()); 
+//	    // Update RatingBar
+//	    RatingBar rating = (RatingBar)v.findViewById(R.id.ColratingBar);
+//	    rating.setEnabled(true); // Enabled
+//	    rating.setRating(Float.valueOf(newRatingPoint));
+//	    rating.setEnabled(false); // False
+//	}
 
 }
