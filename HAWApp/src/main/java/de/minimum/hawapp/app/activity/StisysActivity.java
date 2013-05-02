389103f3e99fef156457414c5e7821e45e1c6a8f
blubbay@gmail.com <blubbay@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-05-02 17:52:15 +0000

    Stisys update (Encoding fehler behoben)

--- HAWApp/src/main/java/de/minimum/hawapp/app/activity/StisysActivity.java
+++ HAWApp/src/main/java/de/minimum/hawapp/app/activity/StisysActivity.java
@@ -19,107 +19,101 @@ import de.minimum.hawapp.app.stisys.Result;
 import de.minimum.hawapp.app.stisys.StisysImpl;
 
 public class StisysActivity extends ExpandableListActivity {
-	private static final String NAME = "NAME";
-	private static final String DESCRIPTION = "DESCRIPTION";
+    private static final String NAME = "NAME";
+    private static final String DESCRIPTION = "DESCRIPTION";
 
-	private ExpandableListAdapter mAdapter;
+    private ExpandableListAdapter mAdapter;
 
-	protected static String login = "";
-	protected static String password = "";
-	private static Map<Integer, List<Result>> results;
-	public static final int DIALOG_DOWNLOAD_JSON_PROGRESS = 0;
+    protected static String login = "";
+    protected static String password = "";
+    private static Map<Integer, List<Result>> results;
+    public static final int DIALOG_DOWNLOAD_INFORMATION_PROGRESS = 0;
 
-	@Override
-	protected Dialog onCreateDialog(int id) {
-		ProgressDialog mProgressDialog;
-		switch (id) {
-		case DIALOG_DOWNLOAD_JSON_PROGRESS:
-			mProgressDialog = new ProgressDialog(this);
-			mProgressDialog.setMessage("Downloading.....");
-			mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-			mProgressDialog.setCancelable(true);
-			mProgressDialog.show();
-			return mProgressDialog;
-		default:
-			return null;
-		}
-	}
+    @Override
+    protected Dialog onCreateDialog(final int id) {
+        ProgressDialog mProgressDialog;
+        switch(id) {
+            case DIALOG_DOWNLOAD_INFORMATION_PROGRESS:
+                mProgressDialog = new ProgressDialog(this);
+                mProgressDialog.setMessage("Loading Information...");
+                mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                mProgressDialog.setCancelable(true);
+                mProgressDialog.show();
+                return mProgressDialog;
+            default:
+                return null;
+        }
+    }
 
-	@Override
-	public void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		showDialog(DIALOG_DOWNLOAD_JSON_PROGRESS);
-		final ParseTask parseTask = new ParseTask();
-		parseTask.execute(login, password);
+        showDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+        final ParseTask parseTask = new ParseTask();
+        parseTask.execute(login, password);
+    }
 
-	};
+    @Override
+    protected void onResume() {
+        super.onResume();
+    };
 
-	private void fillFields() {
-		final List<Map<String, String>> groupData = new ArrayList<Map<String, String>>();
-		final List<List<Map<String, String>>> childData = new ArrayList<List<Map<String, String>>>();
+    private void generateStisysUI() {
+        final List<Map<String, String>> groupData = new ArrayList<Map<String, String>>();
+        final List<List<Map<String, String>>> childData = new ArrayList<List<Map<String, String>>>();
 
-		final Set<Integer> resultKeySet = results.keySet();
-		final List<Integer> resultKeyList = new ArrayList<Integer>(resultKeySet);
-		Collections.sort(resultKeyList);
+        final Set<Integer> resultKeySet = results.keySet();
+        final List<Integer> resultKeyList = new ArrayList<Integer>(resultKeySet);
+        Collections.sort(resultKeyList);
 
-		for (final Integer i : resultKeyList) {
-			final Map<String, String> curGroupMap = new HashMap<String, String>();
-			groupData.add(curGroupMap);
-			curGroupMap.put(NAME, "B-AI " + i);
+        for(final Integer i : resultKeyList) {
+            final Map<String, String> curGroupMap = new HashMap<String, String>();
+            groupData.add(curGroupMap);
+            curGroupMap.put(NAME, "B-AI " + i);
 
-			final List<Result> listResult = results.get(i);
-			final List<Map<String, String>> children = new ArrayList<Map<String, String>>();
-			for (final Result r : listResult) {
-				final Map<String, String> curChildMap = new HashMap<String, String>();
-				children.add(curChildMap);
-				curChildMap.put(NAME, r.getName());
-				curChildMap.put(DESCRIPTION, r.getDate() + " " + r.getProf()
-						+ " " + r.getErgebnis());
-			}
-			childData.add(children);
-		}
+            final List<Result> listResult = results.get(i);
+            final List<Map<String, String>> children = new ArrayList<Map<String, String>>();
+            for(final Result r : listResult) {
+                final Map<String, String> curChildMap = new HashMap<String, String>();
+                children.add(curChildMap);
+                curChildMap.put(NAME, r.getName());
+                curChildMap.put(DESCRIPTION, r.getDate() + " " + r.getProf() + " " + r.getErgebnis());
+            }
+            childData.add(children);
+        }
 
-		// Set up our adapter
-		mAdapter = new SimpleExpandableListAdapter(this, groupData,
-				R.layout.simple_expandable_list_item_1, new String[] { NAME,
-						DESCRIPTION }, new int[] { android.R.id.text1,
-						android.R.id.text2 }, childData,
-				R.layout.simple_expandable_list_item_2, new String[] { NAME,
-						DESCRIPTION }, new int[] { android.R.id.text1,
-						android.R.id.text2 });
-		setListAdapter(mAdapter);
-	}
+        // Set up our adapter
+        mAdapter = new SimpleExpandableListAdapter(this, groupData, R.layout.simple_expandable_list_item_1,
+                        new String[] { NAME, DESCRIPTION }, new int[] { android.R.id.text1, android.R.id.text2 },
+                        childData, R.layout.simple_expandable_list_item_2, new String[] { NAME, DESCRIPTION },
+                        new int[] { android.R.id.text1, android.R.id.text2 });
+        setListAdapter(mAdapter);
+    }
 
-	class ParseTask extends AsyncTask<String, Void, Map<Integer, List<Result>>> {
-		@Override
-		protected Map<Integer, List<Result>> doInBackground(
-				final String... params) {
-			final String login = params[0];
-			// Log.i("StisysActivity LoginTask", "Login: " + login);
-			final String password = params[1];
-			// Log.i("StisysActivity Lo
+    class ParseTask extends AsyncTask<String, Void, Map<Integer, List<Result>>> {
+        @Override
+        protected Map<Integer, List<Result>> doInBackground(final String... params) {
+            final String login = params[0];
+            // Log.i("StisysActivity LoginTask", "Login: " + login);
+            final String password = params[1];
+            // Log.i("StisysActivity Lo
 
-			final StisysImpl stisys = new StisysImpl();
-			// cookie = stisys.getReportPage(cookie);
+            final StisysImpl stisys = new StisysImpl();
+            // cookie = stisys.getReportPage(cookie);
 
-			final Map<Integer, List<Result>> ret = stisys
-					.parseReportPage(stisys.getReportPage(stisys.login(login,
-							password)));
-			return ret;
-		}
+            final Map<Integer, List<Result>> ret = stisys.parseReportPage(stisys.getReportPage(stisys.login(login,
+                            password)));
+            return ret;
+        }
 
-		@Override
-		protected void onPostExecute(final Map<Integer, List<Result>> result) {
-			results = result;
-			fillFields();
-			dismissDialog(DIALOG_DOWNLOAD_JSON_PROGRESS);
-			removeDialog(DIALOG_DOWNLOAD_JSON_PROGRESS);
-		}
-	}
+        @Override
+        protected void onPostExecute(final Map<Integer, List<Result>> result) {
+            results = result;
+            generateStisysUI();
+            dismissDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+            removeDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+        }
+    }
 
 }
--- HAWApp/src/main/java/de/minimum/hawapp/app/stisys/StisysImpl.java
+++ HAWApp/src/main/java/de/minimum/hawapp/app/stisys/StisysImpl.java
@@ -19,145 +19,138 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 public class StisysImpl implements Stisys {
-	static final String STISYS_URL = "https://stisys.informatik.haw-hamburg.de/stisys/";
-	static final String LOGIN_URL = STISYS_URL + "login.do";
-	static final String REPORT_URL = STISYS_URL + "viewExaminationData.do";
+    static final String STISYS_URL = "https://stisys.informatik.haw-hamburg.de/stisys/";
+    static final String LOGIN_URL = STISYS_URL + "login.do";
+    static final String REPORT_URL = STISYS_URL + "viewExaminationData.do";
 
-	@Override
-	public String login(final String name, final String password) {
-		// Log.i("StisysImpl login", "Login: " + login);
-		// Log.i("StisysImpl login", "Password: " + password);
-		HttpsURLConnection conn = null;
-		List<String> cookies = null;
+    @Override
+    public String login(final String name, final String password) {
+        // Log.i("StisysImpl login", "Login: " + login);
+        // Log.i("StisysImpl login", "Password: " + password);
+        HttpsURLConnection conn = null;
+        List<String> cookies = null;
 
-		try {
-			final URL url = new URL(LOGIN_URL);
-			final String param = "username=" + URLEncoder.encode(name, "UTF-8")
-					+ "&password=" + URLEncoder.encode(password, "UTF-8");
+        try {
+            final URL url = new URL(LOGIN_URL);
+            final String param = "username=" + URLEncoder.encode(name, "UTF-8") + "&password="
+                            + URLEncoder.encode(password, "UTF-8");
 
-			conn = (HttpsURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setFixedLengthStreamingMode(param.getBytes().length);
-			conn.setRequestProperty("Content-Type",
-					"application/x-www-form-urlencoded");
+            conn = (HttpsURLConnection)url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setFixedLengthStreamingMode(param.getBytes().length);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-			final PrintWriter out = new PrintWriter(conn.getOutputStream());
-			out.print(param);
-			out.close();
-			cookies = conn.getHeaderFields().get("Set-Cookie");
-		} catch (final MalformedURLException e) {
-			e.printStackTrace();
-		} catch (final UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (final IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (conn != null) {
-				conn.disconnect();
-			}
-		}
-		return cookies.toString();
-	}
+            final PrintWriter out = new PrintWriter(conn.getOutputStream());
+            out.print(param);
+            out.close();
+            cookies = conn.getHeaderFields().get("Set-Cookie");
+        }
+        catch(final MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch(final UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        catch(final IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return cookies.toString();
+    }
 
-	@Override
-	public String getReportPage(final String usercookie) {
-		HttpsURLConnection conn;
-		URL url;
-		String html = "";
+    @Override
+    public String getReportPage(final String usercookie) {
+        HttpsURLConnection conn;
+        URL url;
+        String html = "";
 
-		try {
-			url = new URL(REPORT_URL);
-			conn = (HttpsURLConnection) url.openConnection();
-			conn.setRequestProperty("Cookie", usercookie);
-			conn.connect();
+        try {
+            url = new URL(REPORT_URL);
+            conn = (HttpsURLConnection)url.openConnection();
+            conn.setRequestProperty("Cookie", usercookie);
+            conn.connect();
 
-			final Scanner inStream = new Scanner(conn.getInputStream());
-			while (inStream.hasNextLine()) {
-				html += (inStream.nextLine());
-			}
-		} catch (final MalformedURLException e) {
-			e.printStackTrace();
-		} catch (final IOException e) {
-			e.printStackTrace();
-		}
-		return html;
-	}
+            final Scanner inStream = new Scanner(conn.getInputStream(), "ISO-8859-1");
+            while(inStream.hasNextLine()) {
+                html += (inStream.nextLine());
+            }
+        }
+        catch(final MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch(final IOException e) {
+            e.printStackTrace();
+        }
+        return html;
+    }
 
-	@Override
-	public Map<Integer, List<Result>> parseReportPage(final String html) {
-		final Map<Integer, List<Result>> ret = new HashMap<Integer, List<Result>>();
-		final Document doc = Jsoup.parse(html);
+    @Override
+    public Map<Integer, List<Result>> parseReportPage(final String html) {
+        final Map<Integer, List<Result>> ret = new HashMap<Integer, List<Result>>();
+        final Document doc = Jsoup.parse(html);
 
-		final Elements tableframe = doc
-				.select("#ergebnisuebersicht .tableframe");
-		final Elements tablecontent = doc
-				.select("#ergebnisuebersicht .tablecontent");
+        final Elements tableframe = doc.select("#ergebnisuebersicht .tableframe");
+        final Elements tablecontent = doc.select("#ergebnisuebersicht .tablecontent");
 
-		for (int i = 0; i < tableframe.size(); i++) {
-			// Log.i("StisysImple",
-			// "Semester: " + tableframe.get(i).select("tr td").text());
-			final List<Result> retList = new ArrayList<Result>();
-			final int semester = Integer
-					.parseInt(tableframe
-							.get(i)
-							.select("tr td")
-							.text()
-							.substring(
-									tableframe.get(i).select("tr td").text()
-											.length() - 1,
-									tableframe.get(i).select("tr td").text()
-											.length()));
+        for(int i = 0; i < tableframe.size(); i++) {
+            // Log.i("StisysImple",
+            // "Semester: " + tableframe.get(i).select("tr td").text());
+            final List<Result> retList = new ArrayList<Result>();
+            final int semester = Integer.parseInt(tableframe
+                            .get(i)
+                            .select("tr td")
+                            .text()
+                            .substring(tableframe.get(i).select("tr td").text().length() - 1,
+                                            tableframe.get(i).select("tr td").text().length()));
 
-			for (int j = 1; j < tablecontent.get(i).select("tr table tr")
-					.size(); j++) {
-				// Log.i("StisysImple",
-				// "Modul: "
-				// + tablecontent.get(i).select("tr table tr")
-				// .get(j).select("td").get(1).text());
-				final String name = tablecontent.get(i).select("tr table tr")
-						.get(j).select("td").get(1).text();
+            for(int j = 1; j < tablecontent.get(i).select("tr table tr").size(); j++) {
+                // Log.i("StisysImple",
+                // "Modul: "
+                // + tablecontent.get(i).select("tr table tr")
+                // .get(j).select("td").get(1).text());
+                final String name = tablecontent.get(i).select("tr table tr").get(j).select("td").get(1).text();
 
-				// Log.i("StisysImple",
-				// "Prof: "
-				// + tablecontent.get(i).select("tr table tr")
-				// .get(j).select("td").get(3).text());
-				final String prof = tablecontent.get(i).select("tr table tr")
-						.get(j).select("td").get(3).text();
+                // Log.i("StisysImple",
+                // "Prof: "
+                // + tablecontent.get(i).select("tr table tr")
+                // .get(j).select("td").get(3).text());
+                final String prof = tablecontent.get(i).select("tr table tr").get(j).select("td").get(3).text();
 
-				// Log.i("StisysImple",
-				// "Datum: "
-				// + tablecontent.get(i).select("tr table tr")
-				// .get(j).select("td").get(5).text());
-				final String date = tablecontent.get(i).select("tr table tr")
-						.get(j).select("td").get(5).text();
+                // Log.i("StisysImple",
+                // "Datum: "
+                // + tablecontent.get(i).select("tr table tr")
+                // .get(j).select("td").get(5).text());
+                final String date = tablecontent.get(i).select("tr table tr").get(j).select("td").get(5).text();
 
-				// Log.i("StisysImple",
-				// "Ergebnis: "
-				// + tablecontent.get(i).select("tr table tr")
-				// .get(j).select("td").get(7).text());
-				final String result = tablecontent.get(i).select("tr table tr")
-						.get(j).select("td").get(7).text();
+                // Log.i("StisysImple",
+                // "Ergebnis: "
+                // + tablecontent.get(i).select("tr table tr")
+                // .get(j).select("td").get(7).text());
+                final String result = tablecontent.get(i).select("tr table tr").get(j).select("td").get(7).text();
 
-				boolean note = false;
-				int k = 0;
-				Result res = null;
-				while (k <= 15 && !note) {
-					if (result.equals(String.valueOf(k))) {
-						res = new ResultPoints(semester, name, prof, date,
-								Integer.valueOf(result));
-						note = true;
-					}
-					k++;
-				}
-				if (!note) {
-					res = new ResultPVL(semester, name, prof, date, result);
-				}
-				retList.add(res);
-			}
-			ret.put(semester, retList);
-		}
-		return ret;
-	}
+                boolean note = false;
+                int k = 0;
+                Result res = null;
+                while(k <= 15 && !note) {
+                    if (result.equals(String.valueOf(k))) {
+                        res = new ResultPoints(semester, name, prof, date, Integer.valueOf(result));
+                        note = true;
+                    }
+                    k++;
+                }
+                if (!note) {
+                    res = new ResultPVL(semester, name, prof, date, result);
+                }
+                retList.add(res);
+            }
+            ret.put(semester, retList);
+        }
+        return ret;
+    }
 
 }
