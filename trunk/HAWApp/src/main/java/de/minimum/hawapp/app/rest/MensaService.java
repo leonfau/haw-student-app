333a455e52afeaf73876927eab2d1a1aca7a6d39
svenallers89@googlemail.com <svenallers89@googlemail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-05-16 09:24:59 +0000

    Erste Version Mensaservice mit Sternerating

--- trunk/HAWApp/src/main/java/de/minimum/hawapp/app/rest/MensaService.java
+++ trunk/HAWApp/src/main/java/de/minimum/hawapp/app/rest/MensaService.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
@@ -16,69 +17,72 @@ import de.minimum.hawapp.app.mensa.beans.DayPlan;
 
 public class MensaService {
 
-	private static final String BASE_URL = "/server/rest/mensaservice/";
-	private final RestTemplate restTemplate;
-	private final List<HttpMessageConverter<?>> messageConverters;
+    private static final String BASE_URL = "/server/rest/mensaservice/";
+    private final RestTemplate restTemplate;
+    private final List<HttpMessageConverter<?>> messageConverters;
 
-	public MensaService() {
-		restTemplate = new RestTemplate();
-		messageConverters = new ArrayList<HttpMessageConverter<?>>();
-		messageConverters.add(new FormHttpMessageConverter());
-		messageConverters.add(new StringHttpMessageConverter());
-		messageConverters.add(new MappingJacksonHttpMessageConverter());
-		restTemplate.setMessageConverters(messageConverters);
-	}
+    public MensaService() {
+        this.restTemplate = new RestTemplate();
+        this.messageConverters = new ArrayList<HttpMessageConverter<?>>();
+        this.messageConverters.add(new FormHttpMessageConverter());
+        this.messageConverters.add(new StringHttpMessageConverter());
+        this.messageConverters.add(new MappingJacksonHttpMessageConverter());
+        this.restTemplate.setMessageConverters(this.messageConverters);
+    }
 
-	/**
-	 * Ermittelt den Speiseplan für einen bestimmten Tag
-	 * 
-	 * @param day
-	 *            Tag für den der Speiseplan ermittelt werden soll
-	 *            ("Montag","Dienstag","Mittwoch","Donnerstag","Freitag")
-	 * @return Eine Liste der Speisen des Tages
-	 */
-	public DayPlan getDayPlan(final String day) {
-		final String url = RestConst.HOST + BASE_URL + "dayplan/" + day;
-		try {
-			return restTemplate.getForObject(url, DayPlan.class);
-		} catch (RestClientException e) {
-			e.printStackTrace();
-			return null;
-		}
+    /**
+     * Ermittelt den Speiseplan für einen bestimmten Tag
+     * 
+     * @param day
+     *            Tag für den der Speiseplan ermittelt werden soll
+     *            ("Montag","Dienstag","Mittwoch","Donnerstag","Freitag")
+     * @return Eine Liste der Speisen des Tages
+     */
+    public DayPlan getDayPlan(final String day) {
+        final String url = RestConst.HOST + MensaService.BASE_URL + "dayplan/" + day;
+        try {
+            return this.restTemplate.getForObject(url, DayPlan.class);
+        }
+        catch(RestClientException e) {
+            e.printStackTrace();
+            return null;
+        }
 
-	}
+    }
 
-	/**
-	 * Ermittelt den Speiseplan für die aktuelle Woche
-	 * 
-	 * @return Eine Map ('Wochentag' => 'Liste der Speisen')
-	 */
-	public List<? extends DayPlan> getWeekPlan() {
-		final String url = RestConst.HOST + BASE_URL + "weekplan";
-		try {
-			return Arrays.asList(restTemplate
-					.getForObject(url, DayPlan[].class));
-		} catch (RestClientException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+    /**
+     * Ermittelt den Speiseplan für die aktuelle Woche
+     * 
+     * @return Eine Map ('Wochentag' => 'Liste der Speisen')
+     */
+    public List<? extends DayPlan> getWeekPlan() {
+        final String url = RestConst.HOST + MensaService.BASE_URL + "weekplan";
+        try {
+            return Arrays.asList(this.restTemplate.getForObject(url, DayPlan[].class));
+        }
+        catch(RestClientException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-	public void ratePositive(UUID mealId) {
-		final String url = RestConst.HOST + BASE_URL + "positiverate/" + mealId;
-		try {
-			restTemplate.put(url, null);
-		} catch (RestClientException e) {
-			e.printStackTrace();
-		}
-	}
+    public void ratePositive(UUID mealId) {
+        final String url = RestConst.HOST + MensaService.BASE_URL + "rate/" + mealId + "/stars/" + 5;
+        try {
+            this.restTemplate.postForObject(url, null, ResponseEntity.class);
+        }
+        catch(RestClientException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void rateNegative(UUID mealId) {
-		final String url = RestConst.HOST + BASE_URL + "negativerate/" + mealId;
-		try {
-			restTemplate.put(url, null);
-		} catch (RestClientException e) {
-			e.printStackTrace();
-		}
-	}
+    public void rateNegative(UUID mealId) {
+        final String url = RestConst.HOST + MensaService.BASE_URL + "negativerate/" + mealId;
+        try {
+            this.restTemplate.put(url, null);
+        }
+        catch(RestClientException e) {
+            e.printStackTrace();
+        }
+    }
 }
