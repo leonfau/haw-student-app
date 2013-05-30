9d1d789ac15c53b7b40d913d6a646117c118833f
leon.fausten@gmail.com <leon.fausten@gmail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-05-30 15:37:38 +0000

    *implement event datatype *add methods to add, update, delete events

--- trunk/HAWApp/src/main/java/de/minimum/hawapp/app/calendar/provider/CalendarProvider.java
+++ trunk/HAWApp/src/main/java/de/minimum/hawapp/app/calendar/provider/CalendarProvider.java
@@ -1,9 +1,37 @@
 package de.minimum.hawapp.app.calendar.provider;
 
+import java.util.List;
 import java.util.Set;
 
 public interface CalendarProvider {
-	
+	/**
+	 * Calender mit folgenden Infos:
+	 *  ID
+	 *	ACCOUNT_NAME
+	 *	DISPLAY_NAME
+	 * 
+	 */
 	public Set<Calendar> getCalendars();
 	
+	/**
+	 * 
+	 * @param event, must have startMillis, endMillis
+	 * @param calendar, musst have id 
+	 */
+	public long addEvent(Calendar calendar, Event event);
+	public List<Long> addEvents(Calendar calendar, List<Event> events);
+	
+	
+	/**
+	 * 
+	 * @param event must have event id;
+	 */
+	public void deleteEvent(Event event);
+	public void deleteEvents(List<Event> events);
+	
+	/**
+	 * 
+	 * @param event must have event id;
+	 */
+	public void updateEvent(Event event);
 }
