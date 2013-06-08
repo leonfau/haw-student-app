9839c967bff1131de3ba159bc28d7a3526789592
svenallers89@googlemail.com <svenallers89@googlemail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-06-08 12:47:51 +0000

    Eigene und ignorierte Angebot werden nun dauerhaft im Speicher abgelegt

--- trunk/HAWApp/src/test/java/de/minimum/hawapp/app/blackboard/DefaultBlackboardManagerTest.java
+++ trunk/HAWApp/src/test/java/de/minimum/hawapp/app/blackboard/DefaultBlackboardManagerTest.java
@@ -3,48 +3,45 @@ package de.minimum.hawapp.app.blackboard;
 import java.util.List;
 
 import junit.framework.Assert;
-import junit.framework.TestCase;
-import android.content.Context;
 import android.test.AndroidTestCase;
 import de.minimum.hawapp.app.blackboard.api.BlackboardManager;
 import de.minimum.hawapp.app.blackboard.api.Category;
 import de.minimum.hawapp.app.blackboard.api.Offer;
 import de.minimum.hawapp.app.blackboard.exceptions.OfferCreationFailedException;
 
-public class DefaultBlackboardManagerTest extends TestCase {
+public class DefaultBlackboardManagerTest extends AndroidTestCase {
 
-    private Context context = new AndroidTestCase().getContext();
     private BlackboardManager bbManager = new DefaultBlackboardManager();
 
     public void testIgnore() {
-        List<Offer> offers = this.bbManager.getAllOffers(this.context);
-        List<Offer> ignored = this.bbManager.getIgnoredOffers(this.context);
+        List<Offer> offers = this.bbManager.getAllOffers(getContext());
+        List<Offer> ignored = this.bbManager.getIgnoredOffers(getContext());
         Offer toIgnore = getOfferNotInIgnore(offers, ignored);
         if (toIgnore == null)
             return;
-        Category cat = this.bbManager.getCategory(this.context, toIgnore.getCategoryName());
+        Category cat = this.bbManager.getCategory(getContext(), toIgnore.getCategoryName());
         Assert.assertTrue("Offer ist in Cat", cat.getAllOffers().contains(toIgnore));
 
-        this.bbManager.unignoreOffer(this.context, toIgnore);
-        List<Offer> offersAfterSenselessUnignore = this.bbManager.getAllOffers(this.context);
-        Category catAfterSenselessUnignore = this.bbManager.getCategory(this.context, toIgnore.getCategoryName());
+        this.bbManager.unignoreOffer(getContext(), toIgnore);
+        List<Offer> offersAfterSenselessUnignore = this.bbManager.getAllOffers(getContext());
+        Category catAfterSenselessUnignore = this.bbManager.getCategory(getContext(), toIgnore.getCategoryName());
         Assert.assertEquals("Liste größe stimmt", offers.size(), offersAfterSenselessUnignore.size());
         Assert.assertEquals("Cat größe stimmt.", cat.getAllOffers().size(), catAfterSenselessUnignore.getAllOffers()
                         .size());
         Assert.assertTrue("Offer ist immernoch in Liste", offersAfterSenselessUnignore.contains(toIgnore));
         Assert.assertTrue("Offer ist immernoch in Cat", catAfterSenselessUnignore.getAllOffers().contains(toIgnore));
 
-        this.bbManager.ignoreOffer(this.context, toIgnore);
-        List<Offer> offersAfterIgnore = this.bbManager.getAllOffers(this.context);
-        Category catAfterIgnore = this.bbManager.getCategory(this.context, toIgnore.getCategoryName());
+        this.bbManager.ignoreOffer(getContext(), toIgnore);
+        List<Offer> offersAfterIgnore = this.bbManager.getAllOffers(getContext());
+        Category catAfterIgnore = this.bbManager.getCategory(getContext(), toIgnore.getCategoryName());
         Assert.assertEquals("Liste größe stimmt", offers.size() - 1, offersAfterIgnore.size());
         Assert.assertEquals("Cat größe stimmt.", cat.getAllOffers().size() - 1, catAfterIgnore.getAllOffers().size());
         Assert.assertFalse("Offer ist nicht mehr in Liste", offersAfterIgnore.contains(toIgnore));
         Assert.assertFalse("Offer ist nicht mehr in Cat", catAfterIgnore.getAllOffers().contains(toIgnore));
 
-        this.bbManager.ignoreOffer(this.context, toIgnore);
-        List<Offer> offersAfterSenselessIgnore = this.bbManager.getAllOffers(this.context);
-        Category catAfterSenselessIgnore = this.bbManager.getCategory(this.context, toIgnore.getCategoryName());
+        this.bbManager.ignoreOffer(getContext(), toIgnore);
+        List<Offer> offersAfterSenselessIgnore = this.bbManager.getAllOffers(getContext());
+        Category catAfterSenselessIgnore = this.bbManager.getCategory(getContext(), toIgnore.getCategoryName());
         Assert.assertEquals("Liste größe stimmt", offers.size() - 1, offersAfterSenselessIgnore.size());
         Assert.assertEquals("Cat größe stimmt.", cat.getAllOffers().size() - 1, catAfterSenselessIgnore.getAllOffers()
                         .size());
@@ -52,9 +49,9 @@ public class DefaultBlackboardManagerTest extends TestCase {
         Assert.assertFalse("Offer ist immernoch nicht mehr in Cat",
                         catAfterSenselessIgnore.getAllOffers().contains(toIgnore));
 
-        this.bbManager.unignoreOffer(this.context, toIgnore);
-        List<Offer> offersAfterUnignore = this.bbManager.getAllOffers(this.context);
-        Category catAfterUnignore = this.bbManager.getCategory(this.context, toIgnore.getCategoryName());
+        this.bbManager.unignoreOffer(getContext(), toIgnore);
+        List<Offer> offersAfterUnignore = this.bbManager.getAllOffers(getContext());
+        Category catAfterUnignore = this.bbManager.getCategory(getContext(), toIgnore.getCategoryName());
         Assert.assertEquals("Liste größe stimmt", offers.size(), offersAfterUnignore.size());
         Assert.assertEquals("Cat größe stimmt.", cat.getAllOffers().size(), catAfterUnignore.getAllOffers().size());
         Assert.assertTrue("Offer wieder in Liste", offersAfterUnignore.contains(toIgnore));
@@ -72,26 +69,26 @@ public class DefaultBlackboardManagerTest extends TestCase {
     public void testCreate() throws OfferCreationFailedException {
         boolean exceptionThrown = false;
         try {
-            this.bbManager.createOffer(this.context, "svbbvhs", "Fail", "Bla", "blubb", null);
+            this.bbManager.createOffer(getContext(), "svbbvhs", "Fail", "Bla", "blubb", null);
         }
         catch(OfferCreationFailedException e) {
             exceptionThrown = true;
         }
         Assert.assertTrue("Offer konnte nicht erstellt werden.", exceptionThrown);
 
-        List<String> catNames = this.bbManager.getAllCategoryNames(this.context);
+        List<String> catNames = this.bbManager.getAllCategoryNames(getContext());
         String category = catNames.get(0);
         String header = "Testüberschrift";
         String description = "Ich verkaufe etwas ganz tolles";
         String contact = "Such mich";
-        Long id = this.bbManager.createOffer(this.context, category, header, description, contact, null);
+        Long id = this.bbManager.createOffer(getContext(), category, header, description, contact, null);
 
-        Offer o = this.bbManager.getOfferById(this.context, id);
+        Offer o = this.bbManager.getOfferById(getContext(), id);
         Assert.assertNotNull("Offer existiert", o);
-        Assert.assertTrue("Offer in Category vorhanden.", this.bbManager.getCategory(this.context, category)
+        Assert.assertTrue("Offer in Category vorhanden.", this.bbManager.getCategory(getContext(), category)
                         .getAllOffers().contains(o));
-        Assert.assertTrue("Offer in Eigenen vorhanden.", this.bbManager.getAllOwnOffers(this.context).contains(o));
-        Assert.assertTrue("Offer in allen Offers vorhanden.", this.bbManager.getAllOffers(this.context).contains(o));
+        Assert.assertTrue("Offer in Eigenen vorhanden.", this.bbManager.getAllOwnOffers(getContext()).contains(o));
+        Assert.assertTrue("Offer in allen Offers vorhanden.", this.bbManager.getAllOffers(getContext()).contains(o));
         Assert.assertEquals("Id richtig", (long)id, o.getId());
         Assert.assertEquals("Category richtig", category, o.getCategoryName());
         Assert.assertEquals("Header richtig", header, o.getHeader());
@@ -100,15 +97,15 @@ public class DefaultBlackboardManagerTest extends TestCase {
         Assert.assertNotNull("Datum gesetzt", o.getDateOfCreation());
         Assert.assertNull("Image ist null", o.getImageId());
 
-        Assert.assertTrue("Entfernen gelingt", this.bbManager.removeOwnOffer(this.context, o));
+        Assert.assertTrue("Entfernen gelingt", this.bbManager.removeOwnOffer(getContext(), o));
         Assert.assertFalse("Offer nicht mehr in Category vorhanden.", this.bbManager
-                        .getCategory(this.context, category).getAllOffers().contains(o));
-        Assert.assertFalse("Offer nicht mehr in Eigenen vorhanden.", this.bbManager.getAllOwnOffers(this.context)
+                        .getCategory(getContext(), category).getAllOffers().contains(o));
+        Assert.assertFalse("Offer nicht mehr in Eigenen vorhanden.", this.bbManager.getAllOwnOffers(getContext())
                         .contains(o));
-        Assert.assertFalse("Offer nicht mehr in allen Offers vorhanden.", this.bbManager.getAllOffers(this.context)
+        Assert.assertFalse("Offer nicht mehr in allen Offers vorhanden.", this.bbManager.getAllOffers(getContext())
                         .contains(o));
-        Assert.assertNull("Offer nicht mehr holbar.", this.bbManager.getOfferById(this.context, id));
+        Assert.assertNull("Offer nicht mehr holbar.", this.bbManager.getOfferById(getContext(), id));
 
-        Assert.assertFalse("Entfernen gelingt nicht mehr", this.bbManager.removeOwnOffer(this.context, o));
+        Assert.assertFalse("Entfernen gelingt nicht mehr", this.bbManager.removeOwnOffer(getContext(), o));
     }
 }
