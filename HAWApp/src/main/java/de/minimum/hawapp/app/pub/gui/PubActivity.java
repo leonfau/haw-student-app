654f29d1371627e27157415a2daac8bfa816fbbf
e.lang160@googlemail.com <e.lang160@googlemail.com@6d9881bc-d824-6923-444b-8602433b50e2>
2013-06-13 16:19:39 +0000

    Pub: Bugfixes, Fehlerbehandlung und kleine Dinge an der GUI

--- HAWApp/src/main/java/de/minimum/hawapp/app/pub/gui/PubActivity.java
+++ HAWApp/src/main/java/de/minimum/hawapp/app/pub/gui/PubActivity.java
@@ -20,239 +20,271 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.jcraft.jsch.SftpException;
 
 import de.minimum.hawapp.app.R;
 import de.minimum.hawapp.app.context.ManagerFactory;
 import de.minimum.hawapp.app.pub.beans.FTPFile;
+import de.minimum.hawapp.app.pub.beans.UpperDirFTPFile;
 import de.minimum.hawapp.app.pub.management.PubManager;
 import de.minimum.hawapp.app.util.InternetConnectionUtil;
 
 public class PubActivity extends Activity {
 
-    PubManager manager;
+	PubManager manager;
 
-    private final List<FTPFile> listItems = new ArrayList<FTPFile>();
-    private List<FTPFile> favorites = new ArrayList<FTPFile>();
-    private PubAdapter adapter;
-    private ListView lv;
-    private String loading;
+	private final List<FTPFile> listItems = new ArrayList<FTPFile>();
+	private List<FTPFile> favorites = new ArrayList<FTPFile>();
+	private PubAdapter adapter;
+	private ListView lv;
+	private String loading;
 
-    private FTPFile currentDirectory;
-    public static final int DIALOG_DOWNLOAD_INFORMATION_PROGRESS = 0;
+	private FTPFile currentDirectory;
+	public static final int DIALOG_DOWNLOAD_INFORMATION_PROGRESS = 0;
 
-    @Override
-    protected Dialog onCreateDialog(final int id) {
-        ProgressDialog mProgressDialog;
-        switch(id) {
-            case DIALOG_DOWNLOAD_INFORMATION_PROGRESS:
-                mProgressDialog = new ProgressDialog(this);
-                mProgressDialog.setMessage(loading + " Pub...");
-                mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-                mProgressDialog.setCancelable(true);
-                mProgressDialog.show();
-                return mProgressDialog;
-            default:
-                return null;
-        }
-    }
+	private final String NO_INTERNET_CONNECTION_MSG = "Keine Internetverbindung vorhanden";
+	private final String ALREADY_BUSY_MSG = "Es wird bereits etwas heruntergeladen";
+	
+	private boolean busyDownloadung = false;
 
-    @Override
-    public void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pub);
-        loading = getString(R.string.loading);
+	@Override
+	protected Dialog onCreateDialog(final int id) {
+		ProgressDialog mProgressDialog;
+		switch (id) {
+		case DIALOG_DOWNLOAD_INFORMATION_PROGRESS:
+			mProgressDialog = new ProgressDialog(this);
+			mProgressDialog.setMessage(loading + " Pub...");
+			mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			mProgressDialog.setCancelable(true);
+			mProgressDialog.show();
+			return mProgressDialog;
+		default:
+			return null;
+		}
+	}
 
-        manager = ManagerFactory.getManager(PubManager.class);
-        favorites = manager.loadFavorites();
-        adapter = new PubAdapter(this, R.layout.activity_pub_item_row, listItems);
-        lv = (ListView)findViewById(R.id.publist);
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pub);
+		loading = getString(R.string.loading);
 
-        lv.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(final AdapterView<?> av, final View view, final int pos, final long id) {
-                showDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
-                handleFile(adapter.getItem(pos));
-            }
-        });
+		manager = ManagerFactory.getManager(PubManager.class);
+		favorites = manager.loadFavorites();
+		adapter = new PubAdapter(this, R.layout.activity_pub_item_row,
+				listItems);
+		lv = (ListView) findViewById(R.id.publist);
 
-        
-        lv.setOnItemLongClickListener(new OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view,
-                    int pos, long id) {
-            	FTPFile clicked = adapter.getItem(pos);
-            	if (clicked.isDirectory()) {
-            		if (favorites.contains(clicked)) {
-                		manager.removeFavorite(clicked);
-                		favorites.remove(clicked);
-            		} else {
-                		manager.makeFavorite(clicked);
-                		favorites.add(clicked);
-            		}
-            		adapter.notifyDataSetChanged();
-            	}
-            	return true;
-            }
-        });
-        
-        
-        lv.setAdapter(adapter);
-        currentDirectory = manager.getRootDirectory();
-        showDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
-        readCurrentDirectory();
-    }
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(final AdapterView<?> av, final View view,
+					final int pos, final long id) {
+				showDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+				handleFile(adapter.getItem(pos));
+			}
+		});
 
-    private void readCurrentDirectory() {
-        new AsyncTask<Void, Void, Void>() {
+		lv.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int pos, long id) {
+				FTPFile clicked = adapter.getItem(pos);
+				if (clicked.isDirectory()) {
+					if (favorites.contains(clicked)) {
+						manager.removeFavorite(clicked);
+						favorites.remove(clicked);
+					} else {
+						manager.makeFavorite(clicked);
+						favorites.add(clicked);
+					}
+					adapter.notifyDataSetChanged();
+				}
+				return true;
+			}
+		});
 
-            List<FTPFile> filesFromDir;
+		lv.setAdapter(adapter);
+		if (InternetConnectionUtil
+				.hasInternetConnection(getApplicationContext())) {
+			currentDirectory = manager.getRootDirectory();
+			TextView pathView = (TextView) findViewById(R.id.path);
+			pathView.setText(currentDirectory.getAbsolutePath()
+					+ currentDirectory.getFileName());
+			showDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+			readCurrentDirectory();
+		} else {
+			Toast.makeText(getApplicationContext(), NO_INTERNET_CONNECTION_MSG,
+					Toast.LENGTH_SHORT).show();
+		}
 
-            @Override
-            protected void onPostExecute(final Void arg0) {
-                super.onPostExecute(arg0);
-                updateList(filesFromDir);
-                dismissDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
-                removeDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
-            }
+	}
 
-            @Override
-            protected Void doInBackground(final Void... arg0) {
-                if (InternetConnectionUtil.hasInternetConnection(getApplicationContext())) {
-                    try {
-                        filesFromDir = manager.readDirectory(currentDirectory);
-                    }
-                    catch(final IllegalArgumentException e) {
-                        e.printStackTrace();
-                    }
-                    catch(final SftpException e) {
-                        e.printStackTrace();
-                    }
+	private void readCurrentDirectory() {
+		new AsyncTask<Void, Void, Void>() {
 
-                }
-                else {}
-                return null;
-            }
-        }.execute();
-    }
+			List<FTPFile> filesFromDir;
 
-    private void updateList(final List<FTPFile> files) {
-        listItems.clear();
-        for(final FTPFile file : files) {
-            listItems.add(file);
-        }
-        Collections.sort(listItems);
-        if (!currentDirectory.equals(manager.getRootDirectory())) {
-            listItems.add(0, manager.getUpperDirectory(currentDirectory));
-        }
-        adapter.notifyDataSetChanged();
-        lv.setSelectionAfterHeaderView();
-    }
+			@Override
+			protected void onPostExecute(final Void arg0) {
+				super.onPostExecute(arg0);
+				updateList(filesFromDir);
+				dismissDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+				removeDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+			}
 
-    private void downloadAndRunFile(final FTPFile file) {
-        new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(final Void... arg0) {
+				if (InternetConnectionUtil
+						.hasInternetConnection(getApplicationContext())) {
+					try {
+						filesFromDir = manager.readDirectory(currentDirectory);
+					} catch (final IllegalArgumentException e) {
+						e.printStackTrace();
+					} catch (final SftpException e) {
+						e.printStackTrace();
+					}
 
-            FTPFile downloaded;
+				} else {
+				}
+				return null;
+			}
+		}.execute();
+	}
 
-            @Override
-            protected void onPostExecute(final Void arg0) {
-                super.onPostExecute(arg0);
-                runFileFromActivity(downloaded);
-                dismissDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
-                removeDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
-            }
+	private void updateList(final List<FTPFile> files) {
+		listItems.clear();
+		for (final FTPFile file : files) {
+			listItems.add(file);
+		}
+		Collections.sort(listItems);
+		if (!currentDirectory.equals(manager.getRootDirectory())) {
+			listItems.add(0, manager.getUpperDirectory(currentDirectory));
+		}
+		adapter.notifyDataSetChanged();
+		lv.setSelectionAfterHeaderView();
+	}
 
-            @Override
-            protected Void doInBackground(final Void... arg0) {
-                if (InternetConnectionUtil.hasInternetConnection(getApplicationContext())) {
-                    try {
-                        downloaded = manager.downloadFile(file);
-                    }
-                    catch(final IllegalArgumentException e) {
-                        e.printStackTrace();
-                    }
-                    catch(final SftpException e) {
-                        e.printStackTrace();
-                    }
+	private void downloadAndRunFile(final FTPFile file) {
+		new AsyncTask<Void, Void, Void>() {
 
-                }
-                else {}
-                return null;
-            }
-        }.execute();
-    }
+			FTPFile downloaded;
+			
+			@Override
+			protected void onPostExecute(final Void arg0) {
+				super.onPostExecute(arg0);
+				busyDownloadung = false;
+				runFileFromActivity(downloaded);
+				dismissDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+				removeDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+			}
 
-    private void runFileFromActivity(final FTPFile downloaded) {
-        manager.runFile(downloaded, this);
-    }
+			@Override
+			protected Void doInBackground(final Void... arg0) {
+				if (InternetConnectionUtil
+						.hasInternetConnection(getApplicationContext())) {
+					try {
+						busyDownloadung = true;
+						downloaded = manager.downloadFile(file);
+					} catch (final IllegalArgumentException e) {
+					} catch (final SftpException e) {
+					}
 
-    private void handleFile(final FTPFile clickedFile) {
-        if (clickedFile.isDirectory()) {
-            currentDirectory = clickedFile;
-            readCurrentDirectory();
-        }
-        else {
-            downloadAndRunFile(clickedFile);
-        }
-    }
+				} else {
+				}
+				return null;
+			}
+		}.execute();
+	}
 
-    public class PubAdapter extends ArrayAdapter<FTPFile> {
-        private final int layoutResourceId;
-        private final Context context;
-        private final List<FTPFile> data;
+	private void runFileFromActivity(final FTPFile downloaded) {
+		manager.runFile(downloaded, this);
+	}
 
-        public PubAdapter(final Context context, final int layoutResourceId, final List<FTPFile> data) {
-            super(context, layoutResourceId, data);
-            this.layoutResourceId = layoutResourceId;
-            this.context = context;
-            this.data = data;
-        }
+	private void handleFile(final FTPFile clickedFile) {
+		if (InternetConnectionUtil
+				.hasInternetConnection(getApplicationContext())
+				&& busyDownloadung == false) {
+			if (clickedFile.isDirectory()) {
+				currentDirectory = clickedFile;
+				TextView pathView = (TextView) findViewById(R.id.path);
+				pathView.setText(currentDirectory.getAbsolutePath()
+						+ currentDirectory.getFileName());
+				readCurrentDirectory();
+			} else {
+				downloadAndRunFile(clickedFile);
+			}
+		} else if (busyDownloadung) {
+			Toast.makeText(getApplicationContext(), ALREADY_BUSY_MSG,
+					Toast.LENGTH_SHORT).show();
+		} else {
+			dismissDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+			removeDialog(DIALOG_DOWNLOAD_INFORMATION_PROGRESS);
+			Toast.makeText(getApplicationContext(), NO_INTERNET_CONNECTION_MSG,
+					Toast.LENGTH_SHORT).show();
+		}
 
-        @Override
-        public View getView(final int position, final View convertView, final ViewGroup parent) {
-            View row = convertView;
-            FileHolder holder = null;
+	}
 
-            if (row == null) {
-                final LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-                row = inflater.inflate(layoutResourceId, parent, false);
+	public class PubAdapter extends ArrayAdapter<FTPFile> {
+		private final int layoutResourceId;
+		private final Context context;
+		private final List<FTPFile> data;
 
-                holder = new FileHolder();
-                holder.imgIcon = (ImageView)row.findViewById(R.id.imageViewPub);
-                holder.txtTitle = (TextView)row.findViewById(R.id.textViewPub);
-                holder.favImg = (ImageView)row.findViewById(R.id.favImgPub);
-                row.setTag(holder);
-            }
-            else {
-                holder = (FileHolder)row.getTag();
-            }
+		public PubAdapter(final Context context, final int layoutResourceId,
+				final List<FTPFile> data) {
+			super(context, layoutResourceId, data);
+			this.layoutResourceId = layoutResourceId;
+			this.context = context;
+			this.data = data;
+		}
 
-            final FTPFile file = data.get(position);
-            holder.txtTitle.setText(file.getFileName());
+		@Override
+		public View getView(final int position, final View convertView,
+				final ViewGroup parent) {
+			View row = convertView;
+			FileHolder holder = null;
 
-            if (file.isDirectory()) {
-                holder.imgIcon.setImageResource(R.drawable.directory_icon);
-                if (favorites.contains(file)) {
-                	holder.favImg.setImageResource(R.drawable.star);
-                } else {
-                	holder.favImg.setImageResource(R.drawable.star_empty);
-                }
-                
-            }
-            else {
-                holder.imgIcon.setImageResource(R.drawable.file_icon);
-                holder.favImg.setImageResource(-1);
-            }
+			if (row == null) {
+				final LayoutInflater inflater = ((Activity) context)
+						.getLayoutInflater();
+				row = inflater.inflate(layoutResourceId, parent, false);
 
-            return row;
-        }
-    }
+				holder = new FileHolder();
+				holder.imgIcon = (ImageView) row
+						.findViewById(R.id.imageViewPub);
+				holder.txtTitle = (TextView) row.findViewById(R.id.textViewPub);
+				holder.favImg = (ImageView) row.findViewById(R.id.favImgPub);
+				row.setTag(holder);
+			} else {
+				holder = (FileHolder) row.getTag();
+			}
 
-    static class FileHolder {
-        ImageView imgIcon;
-        TextView txtTitle;
-        ImageView favImg;
-    }
+			final FTPFile file = data.get(position);
+			holder.txtTitle.setText(file.toString());
+			holder.favImg.setImageResource(-1);
+
+			if (file.isDirectory()) {
+				holder.imgIcon.setImageResource(R.drawable.directory_icon);
+				if (!(file instanceof UpperDirFTPFile)) {
+					if (favorites.contains(file)) {
+						holder.favImg.setImageResource(R.drawable.star);
+					} else {
+						holder.favImg.setImageResource(R.drawable.star_empty);
+					}
+				}
+			} else {
+				holder.imgIcon.setImageResource(R.drawable.file_icon);
+			}
+			return row;
+		}
+	}
+
+	static class FileHolder {
+		ImageView imgIcon;
+		TextView txtTitle;
+		ImageView favImg;
+	}
 
 }
--- HAWApp/src/main/java/de/minimum/hawapp/app/pub/management/PubManagerImpl.java
+++ HAWApp/src/main/java/de/minimum/hawapp/app/pub/management/PubManagerImpl.java
@@ -28,110 +28,133 @@ public class PubManagerImpl implements PubManager {
 	private final String server = "sftp.informatik.haw-hamburg.de";
 	private String user;
 	private String pw;
-	
+
 	private final String FTP_TYPE = "sftp";
 	private final String SEPERATOR = "/";
 	private final String URI_PREFIX = "file:/";
-	
+
 	private final String ERROR_ARGUMENT_MUST_BE_FILE = "Argument must be File not Directory";
 	private final String ERROR_ARGUMENT_MUST_BE_DIR = "Argument must be Directory not File";
-	
 
 	private ChannelSftp sftpChannel;
 	private Session session;
-	
+
 	private void connect() {
 		user = Login.decrypt(Login.getEncryptedLogin());
 		pw = Login.decrypt(Login.getEncryptedPassword());
 		JSch jsch = new JSch();
-        session = null;
-        try {
-            session = jsch.getSession(user, server, 22);
-            session.setConfig("StrictHostKeyChecking", "no");
-            session.setPassword(pw);
-            session.connect();
-            Channel channel = session.openChannel(FTP_TYPE);
-            channel.connect();
-            sftpChannel = (ChannelSftp) channel;
-	} catch (JSchException e) {
-        e.printStackTrace();  
+		session = null;
+		try {
+			session = jsch.getSession(user, server, 22);
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.setPassword(pw);
+			session.connect();
+			Channel channel = session.openChannel(FTP_TYPE);
+			channel.connect();
+			sftpChannel = (ChannelSftp) channel;
+		} catch (JSchException e) {
+			e.printStackTrace();
+		}
 	}
-	}
-	
+
 	@Override
 	public FTPFile getRootDirectory() {
 		return new FTPFileBeansImpl("pub", "/home/", true);
 	}
-	
+
 	@Override
 	public FTPFile downloadFile(FTPFile remoteFile)
 			throws IllegalArgumentException, SftpException {
-		if (sftpChannel == null || sftpChannel.isClosed() || !sftpChannel.isConnected()) connect();
-		if (remoteFile.isDirectory()) throw new IllegalArgumentException(ERROR_ARGUMENT_MUST_BE_FILE);
-		
-		String localAbsolutePath = Environment.getExternalStorageDirectory() + SEPERATOR + Environment.DIRECTORY_DOWNLOADS;
-		sftpChannel.get(remoteFile.getAbsolutePath() + remoteFile.getFileName(), localAbsolutePath + remoteFile.getFileName());
-        
-        sftpChannel.exit();
-        session.disconnect();
-		return new FTPFileBeansImpl(remoteFile.getFileName(), localAbsolutePath, false);
+		if (sftpChannel == null || sftpChannel.isClosed()
+				|| !sftpChannel.isConnected())
+			connect();
+		if (remoteFile.isDirectory())
+			throw new IllegalArgumentException(ERROR_ARGUMENT_MUST_BE_FILE);
+
+		String localAbsolutePath = Environment.getExternalStorageDirectory()
+				+ SEPERATOR + Environment.DIRECTORY_DOWNLOADS;
+		sftpChannel.get(
+				remoteFile.getAbsolutePath() + remoteFile.getFileName(),
+				localAbsolutePath + remoteFile.getFileName());
+
+		sftpChannel.exit();
+		session.disconnect();
+		return new FTPFileBeansImpl(remoteFile.getFileName(),
+				localAbsolutePath, false);
 	}
 
 	@Override
 	public List<FTPFile> readDirectory(FTPFile dir)
 			throws IllegalArgumentException, SftpException {
-		if (sftpChannel == null || sftpChannel.isClosed() || !sftpChannel.isConnected()) connect();
-		if (!dir.isDirectory()) throw new IllegalArgumentException(ERROR_ARGUMENT_MUST_BE_DIR);
+		if (sftpChannel == null || sftpChannel.isClosed()
+				|| !sftpChannel.isConnected())
+			connect();
+		if (!dir.isDirectory())
+			throw new IllegalArgumentException(ERROR_ARGUMENT_MUST_BE_DIR);
 		sftpChannel.cd(dir.getAbsolutePath() + dir.getFileName());
-		
-		
-        @SuppressWarnings("unchecked")
+
+		@SuppressWarnings("unchecked")
 		Vector<LsEntry> list = sftpChannel.ls("*");
-        List<FTPFile> resultFiles = new ArrayList<FTPFile>();
-        
-        for(LsEntry ls : list) {
-            resultFiles.add(new FTPFileBeansImpl(ls.getFilename(), dir.getAbsolutePath() + dir.getFileName() + SEPERATOR , ls.getAttrs().isDir() || ls.getAttrs().isLink()));
-        }
-        
-        sftpChannel.exit();
-        session.disconnect();
+		List<FTPFile> resultFiles = new ArrayList<FTPFile>();
+
+		for (LsEntry ls : list) {
+			if (!ls.getFilename().equals("Gesperrt")) {
+				resultFiles.add(new FTPFileBeansImpl(ls.getFilename(), dir
+						.getAbsolutePath() + dir.getFileName() + SEPERATOR, ls
+						.getAttrs().isDir() || ls.getAttrs().isLink()));
+			}
+		}
+
+		sftpChannel.exit();
+		session.disconnect();
 		return resultFiles;
 	}
 
 	@Override
 	public void runFile(FTPFile localFile, Activity fromActivity)
 			throws IllegalArgumentException {
-		if (localFile.isDirectory()) throw new IllegalArgumentException(ERROR_ARGUMENT_MUST_BE_FILE);
-		
-		String file = URI_PREFIX + localFile.getAbsolutePath() + localFile.getFileName();
+		if (localFile.isDirectory())
+			throw new IllegalArgumentException(ERROR_ARGUMENT_MUST_BE_FILE);
+
+		String file = URI_PREFIX + localFile.getAbsolutePath()
+				+ localFile.getFileName();
 		Intent newApp = new Intent(android.content.Intent.ACTION_VIEW);
-	    String extension = MimeTypeMap.getFileExtensionFromUrl(file);
-	    String mimetype = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
-	    newApp.setDataAndType(Uri.parse(file),mimetype);
-	    fromActivity.startActivity(newApp);
+		MimeTypeMap mime = MimeTypeMap.getSingleton();
+		String mimetype = mime.getExtensionFromMimeType("txt");
+		if (file.lastIndexOf(".") != -1) {
+			String ext = file.substring(file.lastIndexOf(".") + 1);
+			mimetype = mime.getMimeTypeFromExtension(ext);
+		}
+		newApp.setDataAndType(Uri.parse(file), mimetype);
 	}
-	
+
+
 	@Override
 	public FTPFile getUpperDirectory(FTPFile dir)
 			throws IllegalArgumentException {
-		if (!dir.isDirectory() || dir.equals(getRootDirectory())) throw new IllegalArgumentException(ERROR_ARGUMENT_MUST_BE_DIR);
+		if (!dir.isDirectory() || dir.equals(getRootDirectory()))
+			throw new IllegalArgumentException(ERROR_ARGUMENT_MUST_BE_DIR);
 		String oldPath = dir.getAbsolutePath();
-		oldPath = oldPath.substring(0,oldPath.length()-1); //letzen Slash abschneiden
-		String path = oldPath.substring(0, oldPath.lastIndexOf(SEPERATOR)+1 ); //bis zum vorletzen Slash
-		String filename = oldPath.substring(oldPath.lastIndexOf(SEPERATOR)+1);
+		oldPath = oldPath.substring(0, oldPath.length() - 1); // letzen Slash
+																// abschneiden
+		String path = oldPath.substring(0, oldPath.lastIndexOf(SEPERATOR) + 1); // bis
+																				// zum
+																				// vorletzen
+																				// Slash
+		String filename = oldPath.substring(oldPath.lastIndexOf(SEPERATOR) + 1);
 		return new UpperDirFTPFile(filename, path, true);
 	}
 
 	@Override
 	public void makeFavorite(FTPFile favoriteDir)
 			throws IllegalArgumentException {
-		//TODO
+		// TODO
 	}
-	
+
 	@Override
 	public void removeFavorite(FTPFile favoriteDir)
 			throws IllegalArgumentException {
-		//TODO
+		// TODO
 	}
 
 	@Override
@@ -139,7 +162,5 @@ public class PubManagerImpl implements PubManager {
 		// TODO Nice to Have
 		return new ArrayList<FTPFile>();
 	}
-
-
 
 }
